31854bdc1a8c450ec412dacc0be38031dbb2b566
zachooz <hoffking@gmail.com>
2014-10-24 10:46:01 -0700

    initial commit

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip myShip;
 
 //your variable declarations here
@@ -131,7 +147,7 @@ abstract class Floater {//Do NOT modify the Floater class! Make changes in the S
 } 
 
 
-void keyPressed(){
+public void keyPressed(){
   
   if (keyCode == UP || key == 87) {
 
@@ -151,5 +167,14 @@ void keyPressed(){
   
   if (key == 32){
     myShip.accelerate(myShip.acceleration);
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
